03b29a0bbb48a12b4da98130036beb64ad4aee46
Ohad Statman <ohads@madgrowth.com>
2024-09-16 11:44:34 +0000

    create js_helper

---
+++ includes/event_config.js
@@ -0,0 +1,11 @@
+
+ function extractEventParam(paramName) {
+  return `
+    (SELECT value.string_value 
+     FROM UNNEST(event_params) 
+     WHERE key = '${paramName}' 
+     LIMIT 1)
+  `;
+}
+module.exports = {
+  extractEventParam}
